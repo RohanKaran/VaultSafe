988a050951ee14b103e2cd2e02bf82e26f261543
Rohan Karan <rohankaran001@gmail.com>
2022-10-05 03:24:55 +0530

    eslint badge

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -103,31 +103,6 @@ export function AuthProvider({ children }) {
 		history.push("/login");
 	};
 
-	// let updateToken = async ()=> {
-
-	//     let response = await fetch('http://127.0.0.1:8000/api/token/refresh/', {
-	//         method:'POST',
-	//         headers:{
-	//             'Content-Type':'application/json'
-	//         },
-	//         body:JSON.stringify({'refresh':authTokens?.refresh})
-	//     })
-
-	//     let data = await response.json()
-
-	//     if (response.status === 200){
-	//         setAuthTokens(data)
-	//         setUser(jwt_decode(data.access))
-	//         localStorage.setItem('authTokens', JSON.stringify(data))
-	//     }else{
-	//         logoutUser()
-	//     }
-
-	//     if(loading){
-	//         setLoading(false)
-	//     }
-	// }
-
 	const contextData = {
 		user,
 		currentUser,
