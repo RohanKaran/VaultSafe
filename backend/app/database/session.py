e93c70d86a2917066f2aca868282c427d1271306
Rohan Karan <rohankaran001@gmail.com>
2022-10-04 23:46:54 +0530

    reg_mail_limit

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -3,9 +3,16 @@ from sqlalchemy.orm import sessionmaker
 
 from ..core import config
 
-engine = create_engine(
-    config.SQLALCHEMY_DATABASE_URI,
-    pool_pre_ping=True,
-    # connect_args={"check_same_thread": False},  # for sqlite
-)
+if config.ENV == "DEV":
+    engine = create_engine(
+        config.SQLALCHEMY_DATABASE_URI,
+        pool_pre_ping=True,
+        connect_args={"check_same_thread": False},  # for sqlite
+    )
+else:
+    engine = create_engine(
+        config.SQLALCHEMY_DATABASE_URI,
+        pool_pre_ping=True,
+        # connect_args={"check_same_thread": False},  # for sqlite
+    )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
