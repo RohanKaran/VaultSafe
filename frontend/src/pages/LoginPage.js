b7981088d56ddcf648c985e24ae2a49367ae76ea
lejara <leoneljara3@gmail.com>
2022-10-06 16:10:10 -0400

    Display loading spinner on log in

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Spinner } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 import { FaEraser, FaSignInAlt } from "react-icons/fa";
@@ -7,66 +7,72 @@ import { Logo } from "../components/Logo";
 import AuthContext from "../context/AuthContext";
 
 function LoginPage() {
-	const { loginUser } = useContext(AuthContext);
+	const { loginUser, loading } = useContext(AuthContext);
 	const [value, setValue] = useState({});
 	return (
 		<div align="center" id="login">
 			<Logo />
-			<Card>
-				<Form
-					onSubmit={loginUser}
-					onChange={() => setValue(value)}
-					onReset={() => setValue({})}
-				>
-					<div align="left">
-						<Form.Group className="mb-3" controlId="formBasicEmail">
-							<Form.Label>Email address</Form.Label>
-							<Form.Control
-								type="email"
-								placeholder="Enter email"
-								name="username"
-								autoComplete="email"
-								required
-							/>
-							<Form.Text className="text-muted">
-                We&apos;ll never share your email with anyone else.
-							</Form.Text>
-						</Form.Group>
+			{!loading ?		
+				<Card>
+					<Form
+						onSubmit={loginUser}
+						onChange={() => setValue(value)}
+						onReset={() => setValue({})}
+					>
+						<div align="left">
+							<Form.Group className="mb-3" controlId="formBasicEmail">
+								<Form.Label>Email address</Form.Label>
+								<Form.Control
+									type="email"
+									placeholder="Enter email"
+									name="username"
+									autoComplete="email"
+									required
+								/>
+								<Form.Text className="text-muted">
+					We&apos;ll never share your email with anyone else.
+								</Form.Text>
+							</Form.Group>
 
-						<Form.Group className="mb-3" controlId="formBasicPassword">
-							<Form.Label>Password</Form.Label>
-							<Form.Control
-								type="password"
-								placeholder="Password"
-								name="password"
-								autoComplete="current-password"
-								required
-							/>
-						</Form.Group>
-						<Form.Group className="mb-3" controlId="formBasicCheckbox">
-							<Form.Check
-								type="checkbox"
-								label="Remember me"
-								name="remember"
-							/>
-						</Form.Group>
+							<Form.Group className="mb-3" controlId="formBasicPassword">
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									type="password"
+									placeholder="Password"
+									name="password"
+									autoComplete="current-password"
+									required
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="formBasicCheckbox">
+								<Form.Check
+									type="checkbox"
+									label="Remember me"
+									name="remember"
+								/>
+							</Form.Group>
+						</div>
+
+						<Button variant="success" type="submit">
+							<FaSignInAlt style={{ marginBottom: 4 }} />
+							{" "}
+				Login
+						</Button>
+						<Button variant="outline-secondary" type="reset">
+							<FaEraser style={{ marginBottom: 4 }} />
+							{" "}
+				Reset
+						</Button>
+					</Form>
+					<div>
+						<Link to="/register">Don&apos;t have an account? Sign up.</Link>
 					</div>
+				</Card>
+				:
+				<Spinner animation="border" role="status" className="mt-5">
+					<span className="visually-hidden">Loading...</span>
+				</Spinner>}
 
-					<Button variant="success" type="submit">
-						<FaSignInAlt style={{ marginBottom: 4 }} />
-						{" "}
-            Login
-					</Button>
-					<Button variant="outline-secondary" type="reset">
-						<FaEraser style={{ marginBottom: 4 }} />
-						{" "}
-            Reset
-					</Button>
-				</Form>
-				<div>
-					<Link to="/register">Don&apos;t have an account? Sign up.</Link>
-				</div>
-			</Card>
 		</div>
 	);
 }
