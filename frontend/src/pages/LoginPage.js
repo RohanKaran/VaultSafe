6834428e4e0dfe5f0546f64274529e32362235e2
lejara <leoneljara3@gmail.com>
2022-10-06 18:08:59 -0400

    Move spinner on top of login button only

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -12,67 +12,69 @@ function LoginPage() {
 	return (
 		<div align="center" id="login">
 			<Logo />
-			{!loading ?		
-				<Card>
-					<Form
-						onSubmit={loginUser}
-						onChange={() => setValue(value)}
-						onReset={() => setValue({})}
-					>
-						<div align="left">
-							<Form.Group className="mb-3" controlId="formBasicEmail">
-								<Form.Label>Email address</Form.Label>
-								<Form.Control
-									type="email"
-									placeholder="Enter email"
-									name="username"
-									autoComplete="email"
-									required
-								/>
-								<Form.Text className="text-muted">
-					We&apos;ll never share your email with anyone else.
-								</Form.Text>
-							</Form.Group>
+			<Card>
+				<Form
+					onSubmit={loginUser}
+					onChange={() => setValue(value)}
+					onReset={() => setValue({})}
+				>
+					<div align="left">
+						<Form.Group className="mb-3" controlId="formBasicEmail">
+							<Form.Label>Email address</Form.Label>
+							<Form.Control
+								type="email"
+								placeholder="Enter email"
+								name="username"
+								autoComplete="email"
+								required
+							/>
+							<Form.Text className="text-muted">
+                We&apos;ll never share your email with anyone else.
+							</Form.Text>
+						</Form.Group>
 
-							<Form.Group className="mb-3" controlId="formBasicPassword">
-								<Form.Label>Password</Form.Label>
-								<Form.Control
-									type="password"
-									placeholder="Password"
-									name="password"
-									autoComplete="current-password"
-									required
-								/>
-							</Form.Group>
-							<Form.Group className="mb-3" controlId="formBasicCheckbox">
-								<Form.Check
-									type="checkbox"
-									label="Remember me"
-									name="remember"
-								/>
-							</Form.Group>
-						</div>
-
-						<Button variant="success" type="submit">
-							<FaSignInAlt style={{ marginBottom: 4 }} />
-							{" "}
-				Login
-						</Button>
-						<Button variant="outline-secondary" type="reset">
-							<FaEraser style={{ marginBottom: 4 }} />
-							{" "}
-				Reset
-						</Button>
-					</Form>
-					<div>
-						<Link to="/register">Don&apos;t have an account? Sign up.</Link>
+						<Form.Group className="mb-3" controlId="formBasicPassword">
+							<Form.Label>Password</Form.Label>
+							<Form.Control
+								type="password"
+								placeholder="Password"
+								name="password"
+								autoComplete="current-password"
+								required
+							/>
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="formBasicCheckbox">
+							<Form.Check
+								type="checkbox"
+								label="Remember me"
+								name="remember"
+							/>
+						</Form.Group>
 					</div>
-				</Card>
-				:
-				<Spinner animation="border" role="status" className="mt-5">
-					<span className="visually-hidden">Loading...</span>
-				</Spinner>}
 
+					<Button variant="success" type="submit" disabled={loading}>
+						{loading ? 			
+							<Spinner animation="border" role="status" size="sm">
+								<span className="visually-hidden">Loading...</span>
+							</Spinner> 
+							:
+							<>
+								<FaSignInAlt style={{ marginBottom: 4 }} />
+								{" "}Login
+							</>				
+						}						
+						
+					</Button>
+					<Button variant="outline-secondary" type="reset">
+						<FaEraser style={{ marginBottom: 4 }} />
+						{" "}
+            Reset
+					</Button>
+				</Form>
+				<div>
+					<Link to="/register">Don&apos;t have an account? Sign up.</Link>
+				</div>
+			</Card>
 		</div>
 	);
 }
