0e9a1c55abeb89ebaff71a582aa04344090bb9b9
rohankaran <rohankaran001@gmail.com>
2026-04-05 17:42:02 +0530

    fix password endpoint router initialization order

--- backend/app/api/endpoints/Password/__init__.py
+++ backend/app/api/endpoints/Password/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter  # pyright: reportUnusedImport=false
 
-from . import controller as controller  # noqa: E402
-
 api = APIRouter()
+
+from . import controller as controller  # noqa: E402
